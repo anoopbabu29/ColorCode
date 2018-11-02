2d8a7c0a57840e6cee11b0ab921c701dfac5faba
anoopbabu29 <anoop.bab@gmail.com>
2018-11-01 23:20:33 -0400

    Made ColorCode a namespace

--- ColorTest.cpp
+++ ColorTest.cpp
@@ -2,15 +2,21 @@
 #include <string>
 #include "ColorCode.hpp"
 
+using namespace std;
+using namespace color;
 
 int main()
 {
-    ColorCode colorOut;
+    ccout.setColors("Bold", "Cyan", "Default");
+    ccout << "Hi There!\n" << "hi\n";
+    ccout.setColors("Underlined", "Red", "Default");
+    ccout << "How are you?\n";
+    ccout << 40;
 
-    colorOut.setColors("Bold", "Cyan", "Default");
-    colorOut << "Hi There!\n" << "hi\n";
-    colorOut << "How are you?\n";
+    ccout.setColors("Bold", "Green", "Default");
+    ccout << "More testing\n";
 
+    cout << "\033[0;33mHi\033[0m" << endl;
 
     return 0;
 }
